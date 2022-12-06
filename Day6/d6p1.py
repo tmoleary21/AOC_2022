07b662dd48de40c1e67e78071254bad8e7199c80
Tyson O'Leary <tmoleary@colostate.edu>
2022-12-05 22:42:43 -0700

    Day 6 puzzle 1

---
+++ Day6/d6p1.py
@@ -0,0 +1,13 @@
+
+with open("input.txt", "r") as f:
+    buffer = f.read()
+
+last_4 = buffer[:4]
+bufferDistance = -1
+for i in range(4,len(buffer)):
+    if len(last_4) == len(set(last_4)):
+        bufferDistance = i
+        break
+    last_4 = last_4[1:] + buffer[i]
+
+print("The number of characters processed before start-of-packet marker is", bufferDistance)
